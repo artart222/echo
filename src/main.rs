e09edcbe58fea7427ee3fc7be52ef61b01692808
Artin Mobasher(artart222) <mobasherartin.icm@gmail.com>
2022-06-12 01:18:39 +0430

    Add -e and -E options for clap

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,39 @@ fn main() {
                 .short('n')
                 .help("do not output the trailing newline"),
         )
+        .arg(
+            Arg::new("E")
+                .short('E')
+                .help("disable interpretation of backslash escapes (default)"),
+        )
+        .arg(Arg::new("e").short('e').help(
+            r#"enable interpretation of backslash escapes
+If -e is in effect, the following sequences are recognized:
+
+\\     backslash
+
+\a     alert (BEL)
+
+\b     backspace
+
+\c     produce no further output
+
+\e     escape
+
+\f     form feed
+
+\n     new line
+
+\r     carriage return
+
+\t     horizontal tab
+
+\v     vertical tab
+
+\0NNN  byte with octal value NNN (1 to 3 digits)
+
+\xHH   byte with hexadecimal value HH (1 to 2 digits)"#,
+        ))
         .get_matches();
 
     let strings: Vec<_> = matches.values_of("STRING(s)").unwrap().collect();
